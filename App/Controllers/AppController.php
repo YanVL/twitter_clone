9cc2a3aeb1439c7ae3c5228acab820b9d26c3325
YanVl <yanvitor.gti@gmail.com>
2022-06-08 20:44:03 -0300

    Criado o metodo de postagem de tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -6,14 +6,32 @@ namespace App\Controllers;
 use MF\Controller\Action;
 use MF\Model\Container;
 
-class AppController extends Action {
+class AppController extends Action
+{
 
-    public function timeline() {
+    public function timeline()
+    {
 
         session_start();
 
-        if($_SESSION['id'] != '' && $_SESSION['nome'] != '')  {
+        if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
             $this->render('timeline');
+        } else {
+            header('Location: /');
+        }
+    }
+
+    public function tweet()
+    {
+
+        session_start();
+
+        if ($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+
+            echo '<pre>';
+            print_r($_POST);
+            echo '</pre>';
+            
         } else {
             header('Location: /');
         }
